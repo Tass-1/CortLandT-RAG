9b5c4bebde71e1e0152e4df0077f7e449cdb6a92
Tass-1 <priyanshujoshi10000@gmail.com>
2026-09-12 21:44:14 +0530

    sec edgar api done

--- Orvan/apps/api/services/celery.py
+++ Orvan/apps/api/services/celery.py
@@ -10,22 +10,39 @@ def fetch_file(ticker:str):
     }
     ticker = ticker.upper()
     ticker_cik = cache.get(ticker)
+    
     if not ticker_cik:
         print("Getting teh cache")
         cik_file = httpx.get("https://www.sec.gov/files/company_tickers.json" , headers=headers)
         if cik_file:
             data = cik_file.json()
             pipe = cache.pipeline()
+            
             for comp in data.values():
                 tic = comp["ticker"].upper()
                 name = str(comp["cik_str"]).zfill(10)
-                pipe.set(tic,name)
+                pipe.set(tic,name,raw_cik)
             pipe.execute()
             ticker_cik = cache.get(ticker)
     if ticker_cik:
         print("getting the location")
         print(ticker_cik)
-        file_location = httpx.get(f'https://data.sec.gov/submissions/CIK{ticker_cik}.json', headers=headers)
-        print(file_location)
-        print(file_location.json())
-    print(cik_file)
+        file_location = httpx.get(f'https://data.sec.gov/submissions/CIK{ticker_cik}.json', headers=headers).json()
+        recent_files = file_location['filings']['recent']
+        tgi = 0
+        for i, form in enumerate(recent_files['form']):
+            if form == "10-K":
+                tgi = i
+                break
+
+        acession = recent_files["accessionNumber"][tgi].replace("-","")
+        doc = recent_files["primaryDocument"][tgi]
+        report = recent_files["reportDate"][tgi]
+        raw_cik = ticker_cik.strip("0")
+        final_file = httpx.get(f'https://www.sec.gov/Archives/edgar/data/{raw_cik}/{acession}/{doc}' , headers= headers).text
+        print(final_file)
+
+
+
+            
+    
